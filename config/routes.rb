ca3f8d9c94dcc0610d2ac9f17b34341a02988bf1
Rui Baltazar <rui.p.baltazar@gmail.com>
2015-02-11 10:30:05 +0800

    Added registrations controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
+
+  namespace :api do
+    namespace :v1 do
+      resources :registrations, only: [:create]
+    end
+  end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
